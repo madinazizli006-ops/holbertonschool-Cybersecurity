852cf72937918194882dab7cd1235c1e15e8e908
root <root@ip-10-42-241-130.ec2.internal>
2026-05-29 19:39:04 +0000

    task

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "%d.%d.%d.%d\n" $(( (0xffffffff << (32-$1) >> 24) & 255 )) $(( (0xffffffff << (32-$1) >> 16) & 255 )) $(( (0xffffffff << (32-$1) >> 8) & 255 )) $(( (0xffffffff << (32-$1)) & 255 ))
+printf "%d.%d.%d.%d" $(( (0xffffffff << (32-$1) >> 24) & 255 )) $(( (0xffffffff << (32-$1) >> 16) & 255 )) $(( (0xffffffff << (32-$1) >> 8) & 255 )) $(( (0xffffffff << (32-$1)) & 255 ))
